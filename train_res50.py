aa2cced73ec9af2f41709928518c3bdde25a1c5f
elia <elia.tosin@student.supsi.ch>
2022-09-17 11:48:30 +0200

    cleaned py train file

--- train_res50.py
+++ train_res50.py
@@ -1,24 +1,18 @@
-# dimensione del batch
+# costants
 BATCH_SIZE = 32
-# n° di epoche del training
 NUM_OF_EPOCHS = 100
-
-# definisce se usare augmentation nel training set
+# boolean if we want to use the augmentation
 USE_AUG = True
-# definisce se loggare i dati in Tensorboard
-log_to_tb = False
-# definisce se usare la ResNet18 (true) o LeNet5 (false)
-using_res = True
-# nome del file .pth che conterrà la rete allenata e l'eventuale cartella di tensorboard
+# name fo the saved model
 NET_NAME = 'resnet50_aug'
 
 SAVE_PATH = 'trained/' + NET_NAME + '.pth'
 SAVE_PATH2 = 'trained/' + NET_NAME + '_best.pth'
-RUNS_PATH = 'runs/' + NET_NAME
-
+# where i can find the dataset
 train_path = 'data/car_brand_logos/Train/'
 test_path = 'data/car_brand_logos/Test/'
 
+#imports
 import torch, torchvision
 from torch.utils.data import Dataset
 from torchvision import datasets
@@ -55,6 +49,7 @@ warnings.filterwarnings("error")
 
 import imgaug.augmenters as iaa
 
+# extract the labels (defined by the folder where the images are)
 def getLabelList(path):
     only_dirs = [ name for name in os.listdir(path) if 
                  os.path.isdir(os.path.join(path, name)) ]
@@ -71,6 +66,7 @@ def getLabelList(path):
     
     return pd.Series(ret)
 
+# match path with labels (int) and classes (string)
 def getImgPool(path):
     only_dirs = [ name for name in os.listdir(path) if 
                  os.path.isdir(os.path.join(path, name)) ]
@@ -89,7 +85,6 @@ def getImgPool(path):
     return pd.Series(ret)
 
 # convert a given format image hidden in JPG format, converting it to a JPG and overwriting the original with its format (WEBP or PNG)
-
 def convertPNGImage(path, start_format):
     img = Image.open(path, formats=[start_format])
     new_path = path[:-3] + 'jpg'
@@ -97,121 +92,121 @@ def convertPNGImage(path, start_format):
     image = Image.open(new_path, formats=['JPEG'])
     return image    
 
-if using_res:
-    class CustomImageDataset(Dataset):
-        def __init__(self, path_labels, transform=None, target_transform=None, use_aug=True):
-            self.img_labels = getLabelList(path_labels)
-            self.images = getImgPool(path_labels)
-            self.transform = transform
-            self.target_transform = target_transform
-            self.use_aug = use_aug
 
-        def __len__(self):
-            return len(self.img_labels)
+class CustomImageDataset(Dataset):
+    def __init__(self, path_labels, transform=None, target_transform=None, use_aug=True):
+        self.img_labels = getLabelList(path_labels)
+        self.images = getImgPool(path_labels)
+        self.transform = transform
+        self.target_transform = target_transform
+        self.use_aug = use_aug
 
-        def __getitem__(self, idx):
-            try: 
-                image = Image.open(self.images.loc[idx][0])
-                if Image.MIME[image.format] == 'image/png': #formato errato
-                        image = convertPNGImage(self.images.loc[idx][0], 'PNG')
-            except Exception:
-                print('Found error at {} in position {}'.format(self.images.loc[idx][0], idx))
-                
-            image = ToTensor()(image)
-            
-            if image.shape[0] != 3: #converto le immagini in bianco e nero e RGBA in RGB
-                image = ToPILImage()(image).convert('RGB')  
-            else: #immagine già RGB
-                image = ToPILImage()(image)
-                
-            preprocess = Compose([
-                Resize(300),
-                CenterCrop(300),
-                ToTensor(),
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        try: 
+            image = Image.open(self.images.loc[idx][0])
+            if Image.MIME[image.format] == 'image/png': #formato errato
+                    image = convertPNGImage(self.images.loc[idx][0], 'PNG')
+        except Exception:
+            print('Found error at {} in position {}'.format(self.images.loc[idx][0], idx))
+
+        image = ToTensor()(image)
+
+        if image.shape[0] != 3: #convert grayscaled e RGBA images in RGB
+            image = ToPILImage()(image).convert('RGB')  
+        else: # RGB image
+            image = ToPILImage()(image)
+
+        preprocess = Compose([
+            Resize(300),
+            CenterCrop(300),
+            ToTensor(),
+        ])
+        image = preprocess(image)
+
+        #parte di data augmentation
+        if self.use_aug:
+            im_np = np.array((image*255).permute(1,2,0), dtype='uint8')
+
+            # augmentations
+            bright = iaa.AddToBrightness((-50,50))
+            rotate = iaa.Rotate((-20, 20))
+            hflip = iaa.Fliplr(0.5)
+            tr_x = iaa.TranslateX(px=(-100, 100))
+            tr_y = iaa.TranslateY(px=(-100, 100))
+            gauss = iaa.imgcorruptlike.GaussianNoise(severity=1)
+            blend = iaa.BlendAlphaHorizontalLinearGradient(iaa.AddToHue((-100, 100)))
+            drop_channel = iaa.Dropout2d(p=0.5)
+            mult = iaa.Multiply((0.5, 1.5), per_channel=0.5)
+
+            seq = iaa.Sequential([
+                iaa.Sometimes(0.5, bright),
+                iaa.Sometimes(0.8, rotate),
+                iaa.Sometimes(0.5, hflip),
+                iaa.Sometimes(0.5, tr_x),
+                iaa.Sometimes(0.5, tr_y),
+                iaa.Sometimes(0.5, gauss),
+                iaa.Sometimes(0.5, blend),
+                iaa.Sometimes(0.5, drop_channel),
+                iaa.Sometimes(0.5, mult)                    
             ])
-            image = preprocess(image)
-            
-            #parte di data augmentation
-            if self.use_aug:
-                im_np = np.array((image*255).permute(1,2,0), dtype='uint8')
 
-                # augmentations
-                bright = iaa.AddToBrightness((-50,50))
-                rotate = iaa.Rotate((-20, 20))
-                hflip = iaa.Fliplr(0.5)
-                tr_x = iaa.TranslateX(px=(-100, 100))
-                tr_y = iaa.TranslateY(px=(-100, 100))
-                gauss = iaa.imgcorruptlike.GaussianNoise(severity=1)
-                blend = iaa.BlendAlphaHorizontalLinearGradient(iaa.AddToHue((-100, 100)))
-                drop_channel = iaa.Dropout2d(p=0.5)
-                mult = iaa.Multiply((0.5, 1.5), per_channel=0.5)
+            img = seq(image=im_np)
+            image = to_tensor(img)
 
-                seq = iaa.Sequential([
-                    iaa.Sometimes(0.5, bright),
-                    iaa.Sometimes(0.8, rotate),
-                    iaa.Sometimes(0.5, hflip),
-                    iaa.Sometimes(0.5, tr_x),
-                    iaa.Sometimes(0.5, tr_y),
-                    iaa.Sometimes(0.5, gauss),
-                    iaa.Sometimes(0.5, blend),
-                    iaa.Sometimes(0.5, drop_channel),
-                    iaa.Sometimes(0.5, mult)                    
-                ])
 
-                img = seq(image=im_np)
-                image = to_tensor(img)
-                
-                
-            
-            preprocess2 = Compose([ # secondo preprocess dell'immagine per evitare problemi con il cambio luminosità
-                Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-            ])
-            image = preprocess2(image)
-            
-            label = self.img_labels.loc[idx][1]
 
-            return image, label
+        preprocess2 = Compose([ # 2nd preprocess to adapt to the pretrained resnet
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        image = preprocess2(image)
+
+        label = self.img_labels.loc[idx][1]
+
+        return image, label
         
 train_data = CustomImageDataset(path_labels=train_path, transform=ToTensor(), target_transform=ToTensor(), use_aug=USE_AUG)
 test_data = CustomImageDataset(path_labels=test_path, transform=ToTensor(), target_transform=ToTensor(), use_aug=False)
 
-print (f'N° immagini nel train dataset: {train_data.__len__()}')
-print (f'N° immagini nel test dataset: {test_data.__len__()}')
+print (f'Num of images in train dataset: {train_data.__len__()}')
+print (f'Num of images in test dataset: {test_data.__len__()}')
 
 train_dl = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
 test_dl = DataLoader(test_data, batch_size=8, shuffle=True)
 
+# checking gpu accelaration (CUDA)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
 print(device)
 
-if using_res:
-    net = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
-    net.to(device)
-    
+# loading the resnet
+net = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
+# editing the output layer to the number of classes
 in_features = net.fc.in_features
 net.fc.out_features = 8
 net.to(device)
     
-#test struttura rete
+#test net loading
 params = list(net.parameters())
 print(len(params))
-print(params[0].size())  # verifico che il primo layer convolutivo sia stato costruito/caricato
+print(params[0].size())
 
 # loss function
 criterion = nn.CrossEntropyLoss()
-# uso stochastic gradient descent
+# stochastic gradient descent
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=NUM_OF_EPOCHS)
 
+# check if loaded on device
 print(next(net.parameters()).device)
 
 best_accuracy = 0
-for epoch in range(NUM_OF_EPOCHS):  # ciclo sul dataset
+for epoch in range(NUM_OF_EPOCHS):  # loop on dataset
     net.train()
     running_loss = 0.0
     for i, data in enumerate(train_dl):
-        # prelevo immagini e label dal dataloader, poi le adatto
+        # picking images from dataloader
         inputs, labels = data
         batch_len = len(labels)
         inputs = inputs.to(device)
@@ -219,14 +214,14 @@ for epoch in range(NUM_OF_EPOCHS):  # ciclo sul dataset
         labels = labels.to(device)
         
         optimizer.zero_grad()
-        outputs = net(inputs) # processo immagini, ottenendo predizioni
-        loss = criterion(outputs, labels) # calcolo loss
+        outputs = net(inputs) # getting preds
+        loss = criterion(outputs, labels) # computing loss
         loss.backward() # backpropagation
         optimizer.step()
 
         running_loss += loss.item()
         skip = 50
-        if i > 0  and i % skip == 0:    # stampa training loss ogni 50 * dimensione batch (50*8 qui)
+        if i > 0  and i % skip == 0:    # print training loss every [skip] batches
             print(f'[{epoch + 1}, {i}/{len(train_dl)}] loss: {(running_loss/skip):.3f}')
             # salvataggio della train loss in tensorboard
             if log_to_tb:
@@ -236,7 +231,7 @@ for epoch in range(NUM_OF_EPOCHS):  # ciclo sul dataset
             running_loss = 0.0
     scheduler.step()
 
-    # fine di un'epoca, calcolo test loss e accuratezza (siccome qui il test dataset è piccolo)
+    # end of the epoch, computing test loss and accuracy
     with torch.no_grad():
         net.eval()
         test_loss = 0
@@ -257,29 +252,21 @@ for epoch in range(NUM_OF_EPOCHS):  # ciclo sul dataset
         actual_test_loss = test_loss/len(test_dl)
         accuracy = (100 * test_acc / test_total)
         
-        # print(f"[{epoch + 1}] test loss: {(actual_test_loss):.3f} , test acc: {accuracy}")
         summary = f"[{epoch + 1}] test loss: {(actual_test_loss):.3f} , test acc: {accuracy}\n"
         with open('metrics.txt', 'a') as f:
             f.write(summary)
         print(summary)
-        if log_to_tb: # salvataggio test loss e accuratezza in tensorboard
-            writer.add_scalar('test loss',
-                           actual_test_loss,
-                           epoch+1)
-            writer.add_scalar('accuracy',
-                           accuracy,
-                           epoch+1)
 
-        # se l'accuratezza migliora, salvo il modello (migliore rispetto a prima)
+        # if we have an improvement in the accuracy, we have a better model
         if accuracy > best_accuracy:
            best_accuracy = accuracy
            torch.save(net.state_dict(), SAVE_PATH2)
         
 
-print('Fine Addestramento')
-print(f"Miglior acc: {best_accuracy:.5f}")
+print('End of training')
+print(f"Best acc: {best_accuracy:.5f}")
 
-#salvo il modello prodotto alla 25a epoca
+#saving the last model (the one obtained at the last epoch)
 torch.save(net.state_dict(), SAVE_PATH)
 
 # nohup /bin/python3 train.py > log.txt 2>&1 &
